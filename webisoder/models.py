56b2abfb66b681780bf48cb20188ed1a184a329c
Stefan Ott <stefan@ott.net>
2014-12-11 02:29:18 +0100

    Added webisoder models

--- webisoder/models.py
+++ webisoder/models.py
@@ -1,27 +1,109 @@
-from sqlalchemy import (
-    Column,
-    Index,
-    Integer,
-    Text,
-    )
+from random import SystemRandom
+from string import digits, ascii_lowercase, ascii_uppercase
 
+from sqlalchemy import Table, ForeignKey, Index
+from sqlalchemy import Boolean, Column, Date, DateTime, Integer, Text
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import relationship, backref
 
-from sqlalchemy.orm import (
-    scoped_session,
-    sessionmaker,
-    )
+from hashlib import md5, sha256
 
 from zope.sqlalchemy import ZopeTransactionExtension
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-
 class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+
+	__tablename__ = 'models'
+	id = Column(Integer, primary_key=True)
+	name = Column(Text)
+	value = Column(Integer)
 
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
+
+subscriptions = Table('subscriptions', Base.metadata,
+	Column('show_id', Integer, ForeignKey('shows.show_id')),
+	Column('user_name', Text, ForeignKey('users.user_name')))
+
+class User(Base):
+
+	__tablename__ = 'users'
+	name = Column('user_name', Text(30), primary_key=True)
+	passwd = Column(Text(64))
+	salt = Column(Text)
+	mail = Column(Text(50), unique=True)
+	signup = Column(DateTime)
+	verified = Column(Boolean)
+	token = Column(Text(12))
+	days_back = Column(Integer)
+	link_provider = Column(Text(20))
+	link_format = Column(Text)
+	site_news = Column(Boolean)
+	lastest_news_read = Column(DateTime)
+	date_offset = Column(Integer)
+	last_login = Column(DateTime)
+
+	def __set_password(self, plain):
+
+		self.salt = ''.join(SystemRandom().choice(ascii_uppercase + 
+			ascii_lowercase + digits) for _ in range(10))
+		self.passwd = sha256((self.salt + plain).encode()).hexdigest()
+
+	def authenticate(self, password):
+
+		digest = sha256((self.salt + password).encode()).hexdigest()
+
+		if digest == self.passwd:
+			return True
+
+		digest = md5(password.encode()).hexdigest()
+		if digest == self.passwd:
+			self.password = password
+			return True
+
+		return False
+
+	def __get_episodes(self):
+
+		shows = [x.id for x in self.shows]
+		return DBSession.query(Episode).filter(Episode.show_id.in_(shows))
+
+	episodes = property(__get_episodes)
+	password = property(None, __set_password)
+
+class Show(Base):
+
+	__tablename__ = 'shows'
+	id = Column('show_id', Integer, primary_key=True)
+	name = Column('show_name', Text)
+	url = Column(Text)
+	updated = Column(DateTime)
+	enabled = Column(Boolean)
+	status = Column(Integer)
+
+	users = relationship(User, secondary=subscriptions, backref='shows')
+
+class Episode(Base):
+
+	__tablename__ = 'episodes'
+	show_id = Column(Integer, ForeignKey('shows.show_id'), primary_key=True)
+	num = Column(Integer, primary_key=True)
+	airdate = Column(Date)
+	season = Column(Integer, primary_key=True)
+	title = Column(Text)
+	totalnum = Column(Integer)
+	prodnum = Column(Text)
+
+	show = relationship(Show, backref='episodes')
+
+	def render(self, format):
+
+		format = format.replace('##SHOW##', self.show.name)
+		format = format.replace('##SEASON##', "%d" % self.season)
+		format = format.replace('##SEASON2##', "%02d" % self.season)
+		format = format.replace('##EPISODE##', "%02d" % self.num)
+		format = format.replace('##TITLE##', "%s" % self.title)
+
+		return format
--- webisoder/tests.py
+++ webisoder/tests.py
@@ -4,7 +4,178 @@ import transaction
 from pyramid import testing
 
 from .models import DBSession
+from .models import Base, Show, Episode, User
 
+class WebisoderModelTests(unittest.TestCase):
+
+	def setUp(self):
+
+		self.config = testing.setUp()
+		from sqlalchemy import create_engine
+		engine = create_engine('sqlite://')
+		DBSession.configure(bind=engine)
+		Base.metadata.create_all(engine)
+
+		with transaction.manager:
+
+			show1 = Show(id=1, name='show1', url='http://1')
+			show2 = Show(id=2, name='show2', url='http://2')
+
+			DBSession.add(show1)
+			DBSession.add(show2)
+
+			DBSession.add(Episode(show=show1, num=1, season=1))
+			DBSession.add(Episode(show=show1, num=2, season=1))
+			DBSession.add(Episode(show=show2, num=1, season=1))
+
+			DBSession.add(User(name='user1'))
+			DBSession.add(User(name='user2'))
+			DBSession.add(User(name='user3'))
+
+	def tearDown(self):
+
+		DBSession.remove()
+		testing.tearDown()
+
+	def test_show_episode_relation(self):
+
+		show1 = DBSession.query(Show).get(1)
+		show2 = DBSession.query(Show).get(2)
+
+		s1e1 = DBSession.query(Episode).filter_by(
+				show=show1, num=1, season=1).first()
+		s1e2 = DBSession.query(Episode).filter_by(
+				show=show1, num=2, season=1).first()
+		s2e1 = DBSession.query(Episode).filter_by(
+				show=show2, num=1, season=1).first()
+
+		self.assertNotEqual(s1e1, s1e2)
+
+		self.assertEqual(2, len(show1.episodes))
+		self.assertIn(s1e1, show1.episodes)
+		self.assertIn(s1e2, show1.episodes)
+
+		self.assertEqual(1, len(show2.episodes))
+		self.assertIn(s2e1, show2.episodes)
+
+		self.assertEqual(s1e1.show, show1)
+		self.assertEqual(s1e2.show, show1)
+		self.assertEqual(s2e1.show, show2)
+
+	def test_subscriptions(self):
+
+		user1 = DBSession.query(User).get('user1')
+		user2 = DBSession.query(User).get('user2')
+		user3 = DBSession.query(User).get('user3')
+
+		show1 = DBSession.query(Show).get(1)
+		show2 = DBSession.query(Show).get(2)
+
+		self.assertEqual(0, len(user1.shows))
+		self.assertEqual(0, len(user2.shows))
+		self.assertEqual(0, len(user3.shows))
+
+		user2.shows.append(show1)
+		user3.shows.append(show1)
+		user3.shows.append(show2)
+
+		user1 = DBSession.query(User).get('user1')
+		user2 = DBSession.query(User).get('user2')
+		user3 = DBSession.query(User).get('user3')
+
+		self.assertEqual(0, len(user1.shows))
+		self.assertEqual(1, len(user2.shows))
+		self.assertEqual(2, len(user3.shows))
+
+		self.assertIn(show1, user2.shows)
+		self.assertIn(show1, user3.shows)
+		self.assertIn(show2, user3.shows)
+
+		self.assertNotIn(user1, show1.users)
+		self.assertIn(user2, show1.users)
+		self.assertIn(user3, show1.users)
+
+		self.assertNotIn(user1, show2.users)
+		self.assertNotIn(user2, show2.users)
+		self.assertIn(user3, show2.users)
+
+	def test_user_episodes(self):
+
+		user1 = DBSession.query(User).get('user1')
+		user2 = DBSession.query(User).get('user2')
+		user3 = DBSession.query(User).get('user3')
+
+		show1 = DBSession.query(Show).get(1)
+		show2 = DBSession.query(Show).get(2)
+
+		user2.shows.append(show1)
+		user3.shows.append(show1)
+		user3.shows.append(show2)
+
+		s1e1 = DBSession.query(Episode).filter_by(
+				show=show1, num=1, season=1).first()
+		s1e2 = DBSession.query(Episode).filter_by(
+				show=show1, num=2, season=1).first()
+		s2e1 = DBSession.query(Episode).filter_by(
+				show=show2, num=1, season=1).first()
+
+		self.assertEqual(0, user1.episodes.count())
+		self.assertEqual(2, user2.episodes.count())
+		self.assertEqual(3, user3.episodes.count())
+
+		self.assertIn(s1e1, user2.episodes.all())
+		self.assertIn(s1e2, user2.episodes.all())
+
+		self.assertIn(s1e1, user3.episodes.all())
+		self.assertIn(s1e2, user3.episodes.all())
+		self.assertIn(s2e1, user3.episodes.all())
+
+		user3.shows.remove(show1)
+		self.assertEqual(1, user3.episodes.count())
+
+	def test_authentication(self):
+
+		user = DBSession.query(User).get('user1')
+		user.salt = ''
+		user.password = 'letmein'
+
+		self.assertTrue(user.authenticate('letmein'))
+		self.assertNotEqual('', user.salt)
+
+	def test_upgrade_password(self):
+
+		user = DBSession.query(User).get('user1')
+		user.passwd = '0d107d09f5bbe40cade3de5c71e9e9b7'
+		user.salt = ''
+
+		self.assertEqual('0d107d09f5bbe40cade3de5c71e9e9b7', user.passwd)
+		self.assertEqual('', user.salt)
+
+		self.assertTrue(user.authenticate('letmein'))
+		self.assertNotEqual('', user.salt)
+		self.assertNotEqual('0d107d09f5bbe40cade3de5c71e9e9b7', user.passwd)
+
+		self.assertTrue(user.authenticate('letmein'))
+
+	def test_render_episode(self):
+
+		show = DBSession.query(Show).get(1)
+
+		ep = Episode(show=show, num=12, season=5, title="test me")
+		fmt = '//##SHOW## ##SEASON##x##EPISODE##'
+		self.assertEqual('//show1 5x12', ep.render(fmt))
+
+		ep = Episode(show=show, num=2, season=5, title="test me")
+		fmt = '//##SHOW## S##SEASON2##E##EPISODE##'
+		self.assertEqual('//show1 S05E02', ep.render(fmt))
+
+		ep = Episode(show=show, num=102, season=1, title="test me")
+		fmt = '//##SHOW## S##SEASON2##E##EPISODE##'
+		self.assertEqual('//show1 S01E102', ep.render(fmt))
+
+		ep = Episode(show=show, num=102, season=1, title="test me")
+		fmt = '//##SHOW## : ##TITLE##'
+		self.assertEqual('//show1 : test me', ep.render(fmt))
 
 class TestMyViewSuccessCondition(unittest.TestCase):
     def setUp(self):
